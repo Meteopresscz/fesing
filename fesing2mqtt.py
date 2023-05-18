632217ea36a1ba32fa30fc829b8572bc766cb404
Tomas "Jethro" Pokorny <xtompok@gmail.com>
2023-05-18 19:56:09 +0200

    Moved reading out data to the function.

--- fesing2mqtt.py
+++ fesing2mqtt.py
@@ -1,54 +1,26 @@
 import sys
 import serial
-import struct
 import time
 import pprint as pp
-from collections import defaultdict
+import logging
 
-from utils import checkcrc, load_specs
+from utils import load_specs, read_all
 
+UNIT_ID = 1
 
-REQUEST = bytes.fromhex("01 18 00 00 00 01 e0 08")
+logging.basicConfig(level=logging.DEBUG)
 
 specs = load_specs("fesing.yaml")
+#pp.pprint(specs)
 
-pp.pprint(specs)
-
-
-def read_param(spec,data,out):
-	if spec['size'] == 1:
-		aformat = "B"
-	elif spec['size'] == 2:
-		aformat = ">H"
-	res = struct.unpack_from(aformat,data,offset=spec['offset'])[0]
-	res += spec.get('shift',0)
-	res *= spec.get('mult',1)
-	out[spec['message']][spec['field']] = res
-
-
-
-ser = serial.Serial(sys.argv[1], baudrate=9600)
-out = defaultdict(dict)
+ser = serial.Serial(sys.argv[1], baudrate=9600, timeout=2)
 
 while True:
-	ser.write(REQUEST)
-	header = ser.read(4)
-	magic,nbytes = struct.unpack(">HH",header)
-	if magic != 0x0118:
-		print(f"Wrong header: {header.hex()[:4]}")
-		time.sleep(5)
-		continue
-	print(f"Will read {nbytes} bytes")
-	data = ser.read(nbytes)
-	print(f"Read {data.hex()}")
-	crc = ser.read(2)
-	print(f"CRC: {crc.hex()}")
-	if checkcrc(header+data+crc):
-		print("CRC OK")
-		for spec in specs:
-			read_param(spec, data, out)
-		pp.pprint(out)
-	else:
-		print("CRC FAIL")
+	try:
+		data = read_all(specs, ser, UNIT_ID)
+		pp.pprint(data)
+	except ValueError as e:
+		print(f"Error in communication: {e}")
+	
 	time.sleep(5)
 
--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 import yaml
 from collections import defaultdict
+import struct
+import logging
+
+logger = logging.getLogger("fesing2mqtt")
 
 
 def modcrc(buf):
@@ -31,3 +35,44 @@ def load_specs(filename):
 		loader = yaml.Loader(f)
 		specs = loader.get_data()
 	return specs
+
+def create_request(aid):
+	header = b"\x01\x18"
+	addr = aid.to_bytes(2,'big')
+	reg = b"\x00\x01"
+	crc = modcrc(header+addr+reg).to_bytes(2,'big')
+	return header+addr+reg+crc
+
+def read_param(spec,data,out):
+	if spec['size'] == 1:
+		aformat = "B"
+	elif spec['size'] == 2:
+		aformat = ">H"
+	res = struct.unpack_from(aformat,data,offset=spec['offset'])[0]
+	res += spec.get('shift',0)
+	res *= spec.get('mult',1)
+	out[spec['message']][spec['field']] = res
+
+def read_all(specs,ser,aid):
+	out = defaultdict(dict)
+	request = create_request(aid)
+	ser.write(request)
+	header = ser.read(4)
+	if len(header) == 0:
+		raise ValueError(f"Did not received any data, is device connected and unit ID set correctly?")
+	if len(header) < 4:
+		raise ValueError(f"Expected 4 bytes, got {len(header)}")
+	magic,nbytes = struct.unpack(">HH",header)
+	if magic != 0x0118:
+		raise ValueError(f"Wrong header: {header.hex()[:4]}")
+	logger.debug(f"Will read {nbytes} bytes")
+	data = ser.read(nbytes)
+	logger.debug(f"Read {data.hex()}")
+	crc = ser.read(2)
+	logger.debug(f"CRC: {crc.hex()}")
+	if not checkcrc(header+data+crc):
+		raise ValueError(f"Wrong CRC")
+	for spec in specs:
+		read_param(spec, data, out)
+	return dict(out)
+
